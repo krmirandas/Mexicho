65f631acf9f4689474186a31b2338d806062ea47
ame_coronado <48495140+amecoronado@users.noreply.github.com>
2019-04-07 23:37:37 -0500

    Add files via upload

--- Mexicho/src/main/java/com/tuukul/proyecto/web/Tema.java
+++ Mexicho/src/main/java/com/tuukul/proyecto/web/Tema.java
@@ -3,19 +3,51 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.tuukul.proyecto.web;
+package com.tuukul.modelo;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
 /**
  *
  * @author coronado
  */
-public class Tema {
-    private int id_tema;
-    private String titulo_tema;
-    private String descripcion_tema;
-    private String color_tema;
-    private int id_usuario;
+
+@Entity
+@Table(catalog="mexicho", schema="temas", name="tema")
+public class Tema implements Serializable {
     
+    @Id
+    @Column (name="id_tema")
+    private int id_tema;
+    
+    @Column(name="titulo_tema")
+    private String titulo_tema;
+    
+    @Column(name="descripcion_tema")
+    private String descripcion_tema;
+    
+    @Column(name="color_tema")
+    private String color_tema; 
+    
+    @Column(name="id_usuario")
+    private int id_usuario;
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
     public int getId_tema() {
         return id_tema;
     }
@@ -47,14 +79,6 @@ public class Tema {
     public void setColor_tema(String color_tema) {
         this.color_tema = color_tema;
     }
-
-    public int getId_usuario() {
-        return id_usuario;
-    }
-
-    public void setId_usuario(int id_usuario) {
-        this.id_usuario = id_usuario;
-    }
-
-
+    
+    
 }
